f04a02f2ce4a9d5f41d230c87c1849f3637754fb
罗庚中 <huanggengzhong@daishujiankang.com>
2023-03-15 20:14:41 +0800

    feat:register

--- controller/userhandler.go
+++ controller/userhandler.go
@@ -1,7 +1,10 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shuwenhe/utils"
+)
 
 func Register(ctx *gin.Context) {
-
+	ctx.JSON(utils.NewSucc())
 }
