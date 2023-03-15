b2ad78cef7f79a749cfe568cbc8997212b34a74e
罗庚中 <huanggengzhong@daishujiankang.com>
2023-03-15 12:52:49 +0800

    feat:ping

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/huanggengzhong/shuwen-gin-vue/controller"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", controller.Ping)
+	r.Run()
+}
