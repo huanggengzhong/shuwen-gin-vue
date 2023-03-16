5f6801d309e6f48a4e1c10d01f43fbac23e16c34
huanggengzhong <915835528@qq.com>
2023-03-17 00:03:54 +0800

    feat:手机校验

--- controller/userhandler.go
+++ controller/userhandler.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/shuwenhe/utils"
 )
@@ -11,6 +13,12 @@ func Register(ctx *gin.Context) {
 	password := ctx.PostForm("password")
 	phone := ctx.PostForm("phone")
 	// 2.验证
+	if len(phone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": "422",
+			"msg":  "手机号长度不对",
+		})
+	}
 	// 3.返回结果
 
 	ctx.JSON(utils.NewSucc("注册成功", gin.H{
